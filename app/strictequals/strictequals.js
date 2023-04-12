14b6a853ef13e13157c4f371d9e7410fba913301
Fede <fnotalvares@gmail.com>
2023-04-12 07:40:08 +0200

    Refactor of strictequal module

--- app/strictequals/strictequals.js
+++ app/strictequals/strictequals.js
@@ -1,30 +1,30 @@
-let valueA = [1, NaN, 0, -0, 1, true, false, "water"];
-let valueB = [1, NaN, -0, 0, "1", false, false, "oil"];
+let valueA = [1, NaN, 0, -0, 1, true, false, "water", "Fede"];
+let valueB = [1, NaN, -0, 0, "1", false, false, "oil", "Fede"];
 
 export const strictEquals = (valueA, valueB) => {
-  const Result = Object.is(valueA, valueB);
+  const result = Object.is(valueA, valueB);
 
-  if (isNaN(valueA) && !valueA) {
-    if (isNaN(valueB) && !valueB) {
-      return !Result;
-    }
+  if (isNaN(valueA) && !valueA && isNaN(valueB) && !valueB) {
+    return !result;
   }
 
-  if (!isNaN(valueA) && !valueA) {
-    if (!isNaN(valueB) && !valueB) {
-      if (Object.is(valueA, valueB)) {
-        return Result;
-      }
-      return !Result;
+  if (!isNaN(valueA) && !valueA && !isNaN(valueB) && !valueB) {
+    if (Object.is(valueA, valueB)) {
+      return result;
     }
+    return !result;
   }
-  return Result;
+  return result;
 };
 
-for (let i = 0; i < 8; i++) {
-  console.log(
-    `For the value A = ${valueA[i]} and B = ${
-      valueB[i]
-    } the result is ${strictEquals(valueA[i], valueB[i])}`
-  );
-}
+const result = () => {
+  for (let i = 0; i < valueA.length; i++) {
+    console.log(
+      `For the value A = ${valueA[i]} and B = ${
+        valueB[i]
+      } the result is ${strictEquals(valueA[i], valueB[i])}`
+    );
+  }
+};
+
+result();
